37763257db2d74ff54e4832c9237e89c5e83239b
IvaNikolov <ivanikolov86@gmail.com>
2022-01-06 21:52:31 +0100

    Inicijalizacija nizova

---
+++ src/Project/PastaBar.java
@@ -0,0 +1,15 @@
+package Project;
+
+import java.util.Scanner;
+
+public class PastaBar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] regularCustomers = { "0631111111", "063222222", "063333333", "064444444", "065555555", "066666666" };
+		String[] ingredients = { "Makarone", "Spagete", "Bolognese", "Curetina", "Govedja prsuta", "Slanina",
+				"Piletina", "4 sira", "Dimljeni sir", "Parmezan", "Pavlaka", "Pesto sos", "Napolitana", "Povrce mix",
+				"Pecurke", "Kutija" };
+		int[] prices = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
+	}
+}
